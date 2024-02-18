79ede63c9c15ef588a707810bb0ace586078bf65
najeebib <najeebibraheem1999@hotmail.com>
2024-02-18 13:50:15 +0200

    finished gardening simulator exercise

--- Week 1/Day 1/Converter.py
+++ Week 1/Day 1/Converter.py
@@ -1,36 +1,36 @@
-userInput = input("Enter your input\nInput format:number units, for example\n10 KG\n").lower().split()
-if len(userInput) != 2:
+user_input = input("Enter your input\nInput format:number units, for example\n10 KG\n").lower().split()
+if len(user_input) != 2:
     print("Invalid input")
 else:
-    value = int(userInput[0])
-    units = userInput[1]
+    value = int(user_input[0])
+    units = user_input[1]
     if units == "f" or units == "c":
         if units == "f":
             converted_value = (value - 32)*(5/9)
-            print(f"Entered {value} F which is {converted_value} C")
+            print(f"Entered {round(value,3)} F which is {round(converted_value,3)} C")
         else:
             converted_value = value*(9/5) + 32
-            print(f"Entered {value} C which is {converted_value} F")
+            print(f"Entered {round(value,3)} C which is {round(converted_value,3)} F")
     elif units == "mph" or units == "kph":
         if units == "kph":
             converted_value = 0.621371 * value
-            print(f"Entered {value} KPH which is {converted_value} MPH")
+            print(f"Entered {value} KPH which is {round(converted_value,3)} MPH")
         else:
             converted_value = 1.609344 * value
-            print(f"Entered {value} MPH which is {converted_value} KPH")
+            print(f"Entered {round(value,3)} MPH which is {round(converted_value,3)} KPH")
     elif units == "kg" or units == "stones" or units == "ibs":
         if units == "kg":
             stones_value = 0.157473 * value
             ibs_value = 2.204622 * value 
-            print(f"Entered {value} Kg which is {stones_value} stones and {ibs_value} pounds")
+            print(f"Entered {round(value,3)} Kg which is {round(stones_value,3)} stones and {round(ibs_value,3)} pounds")
         elif units == "ibs":
             kg_value = 0.453592 *value
             stones_value = 0.0714286 * value
-            print(f"Entered {value} ponds which is {kg_value} Kg and {stones_value} stones")
+            print(f"Entered {round(value,3)} ponds which is {round(kg_value,3)} Kg and {round(stones_value,3)} stones")
         else:
             kg_value = 6.350294 * value
             ibs_value = 14 * value
-            print(f"Entered {value} stones which is {kg_value} Kg and {ibs_value} pounds")
+            print(f"Entered {round(value,3)} stones which is {round(kg_value,3)} Kg and {round(ibs_value,3)} pounds")
     else:
         print("Invalid units entered")
     
