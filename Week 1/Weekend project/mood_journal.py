5018e76e51b3d649fc62ca38079ef70eb9d0961e
najeebib <najeebibraheem1999@hotmail.com>
2024-02-23 12:38:59 +0200

    made some progress
    added the emotion dictionary
    created functions that print all records and one that returns last record
    created a functiont that check if new record and last entry is in the same week
    refactored some code to use regular expressions for finding date instead of negative index

--- Week 1/Weekend project/mood_journal.py
+++ Week 1/Weekend project/mood_journal.py
@@ -1,26 +1,64 @@
 import os
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 
-pattern_str = r'^\d{2}:\d{2}-\d{2}-\d{2}-\d{4}$'
- 
+def read_all_records(file_name):
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            print(line)
+
+def get_last_record(file_name):
+    last = ""
+    with open(file_name, 'r') as f:
+        for line in f.readlines():
+            last = line
+    return last
+
+def is_same_week(mood_str, last_record, date_pattern):
+    mood_date_str = ""
+    date_match = re.search(date_pattern, mood_str)
+    if date_match:
+        mood_date_str = date_match.group()
+    
+    last_record_date_str = ""
+    date_match = re.search(date_pattern, last_record)
+    if date_match:
+        last_record_date_str = date_match.group()
+    date_format = "%d-%m-%Y"
+    try:
+        mood_date = datetime.strptime(mood_date_str, date_format)
+        last_record_date = datetime.strptime(last_record_date_str, date_format)
+
+        week_start_mood = mood_date - timedelta(days=mood_date.weekday())
+        week_start_last = last_record_date - timedelta(days=last_record_date.weekday())
+        return week_start_mood == week_start_last
+
+    except ValueError:
+        print("Invalid date format. Please provide the date in the format:", date_format)
+        
+emotions_dictionary = {"Joy":["happy","excited","delighted","blissful","jubilant"], "Sadness":["unhappy","depressed","sorrowful","mournful","melancholic"]
+                       ,"Anger":["furious","irritated","enraged","annoyed","agitated"], "Fear":["scared","terrified","anxious","nervous","panicked"]
+                       ,"Surprise":["astonished","amazed","startled","shocked","stunned"], "Disgust":["repulsed","revolted","displeased","sickend","appalled"]
+                       ,"Love":["affectionate","adoring","fond","devoted","enamored"], "Confusion":["bewildered","perplexed","baffled","confused","puzzled"]
+                       ,"Contentment":["satisfied","pleased","gratified","content","fullfilled"], "Embarrassment":["ashamed","humiliated","blushing","self-conscious","mortified"]}
+datetime_pattern = r'^\d{2}:\d{2}-\d{2}-\d{2}-\d{4}$'
+date_pattern = r'\d{2}-\d{2}-\d{4}$'
+
 name = input("Enter your name \n")
 file_name = name + ".txt"
 
 mood_string =  input("Enter your current mood\n")
-date_str = mood_string[-16::1]
-print(date_str)
-print(re.match(pattern_str, date_str))
-if re.match(pattern_str, date_str):
-    print(date_str)
+date_match  = re.search(date_pattern, mood_string)
+if date_match:
     mood_string += "\n"
-    with open(file_name, 'a+') as f:
-        f.write(mood_string)
 else:
     now = datetime.now()
     dt_string = now.strftime("%H:%M-%d-%m-%Y")
     mood_string = mood_string + " " + dt_string + "\n"
-    with open(file_name, 'a+') as f:
-        f.write(mood_string)
-
-
+last_record = get_last_record(file_name)
+if is_same_week(mood_string, last_record, date_pattern):
+    print("new entry is in same week as last one")
+else:
+    print("new entry isn't in same week as last one")
+with open(file_name, 'a+') as f:
+    f.write(mood_string)
