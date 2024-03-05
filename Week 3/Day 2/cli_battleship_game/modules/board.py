a6059da64dd02f7c50722acb1fb0d01514aef783
najeebib <najeebibraheem1999@hotmail.com>
2024-03-05 11:55:28 +0200

    finished cli battleship game

--- Week 3/Day 2/cli_battleship_game/modules/board.py
+++ Week 3/Day 2/cli_battleship_game/modules/board.py
@@ -2,8 +2,26 @@ class Board:
     def __init__(self):
         self.tiles = [[None for i in range(5)] for j in range(5)]
         self.ships = []
+        self._ships_num = 5
+        self.tiles_hit = []
 
     def add_ship(self, ship, position):
         self.tiles[position[0]][position[1]] = ship
+
+    def add_hit_at_position(self, position):
+        self.tiles_hit.append(position)
+
     def get_tile(self, position):
         return self.tiles[position[0]][position[1]]
+    
+    def get_all_hit_position(self):
+        return self.tiles_hit
+    
+    def get_ships_num(self):
+        return self._ships_num
+    
+    def decrease_ships_num(self):
+        self._ships_num -= 1
+
+    def __str__(self):
+        return f"There are {self._ships_num} ships on the board"
--- Week 3/Day 2/cli_battleship_game/modules/player.py
+++ Week 3/Day 2/cli_battleship_game/modules/player.py
@@ -4,26 +4,39 @@ class Player:
         self._hits = 0
         self._missed = 0
 
+    def get_rockets(self):
+        return self._rockets
+    
     def fire_rocket(self,position, board):
-        if board.get_tile(position) != None:
-            ship = board.get_tile(position)
-            ship.set_destroyed()
-            self.decrease_rockets()
-            self.increase_hits()
-            print(f"Player hit ship at position {position}")
+        hit_positions = board.get_all_hit_position()
+        if position in hit_positions:
+                print("Can't hit same position twice")
         else:
-            self.decrease_rockets()
-            self.increase_missed()
-            print(f"Player missed at position {position}")
+            board.add_hit_at_position(position)
+            if board.get_tile(position) != None:
+                self.decrease_rockets()
+                self.increase_hits()
+                board.decrease_ships_num()
+                print(f"Player hit ship at position {position}")
+            else:
+                self.decrease_rockets()
+                self.increase_missed()
+                print(f"Player missed at position {position}")
 
     def decrease_rockets(self):
-        self.rockets -= 1
+        self._rockets -= 1
 
     def increase_hits(self):
-        self.hits += 1
+        self._hits += 1
 
     def increase_missed(self):
-        self.missed += 1
+        self._missed += 1
 
+    def get_hits(self):
+        return self._hits
+    
+    def get_missed(self):
+        return self._missed
+    
     def __str__(self):
-        return f"The player has {self._rockets},  {self._hits} had hit a ship and {self._missed} missed its target"
+        return f"The player has {self._rockets} rockets,  {self._hits} rockets had hit a ship and {self._missed} rockets missed its target"
