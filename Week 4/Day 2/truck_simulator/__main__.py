f3f57dcdaa1d8a9924c6f9c421aba94a6bd2c458
najeebib <najeebibraheem1999@hotmail.com>
2024-03-11 14:47:27 +0200

    added logging, print changes to wheel and driver mood

--- Week 4/Day 2/truck_simulator/__main__.py
+++ Week 4/Day 2/truck_simulator/__main__.py
@@ -1,6 +1,10 @@
 from modules.truck import Truck
 from modules.read_data import read_roads_file 
 from traversal_calculator import Traversal_calculator
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w")
+
 def main():
     truck = Truck(20, 50,200,"Toyota")
     calc = Traversal_calculator()
--- Week 4/Day 2/truck_simulator/roads/bumpy.py
+++ Week 4/Day 2/truck_simulator/roads/bumpy.py
@@ -1,8 +1,15 @@
+import logging
+
 def bumpy(distance, truck):
+    
     truck_max_fuel = truck.get_max_fuel_amount()
     truck_km_per_liter = truck.get_km_per_liter()
     road_terrain_hardness = 1.2
+    wheel_damage_effect = 0.008
+    damage_to_wheels = distance * wheel_damage_effect
     max_distance = (truck_max_fuel * truck_km_per_liter) / road_terrain_hardness
+    print(f"Travelling on a bumpy road, the wheels will be damaged by {damage_to_wheels}%, the drivers mood will decrease a little bit")
+    logging.info(f"Travelling on a bumpy road, the wheels will be damaged by {damage_to_wheels}%, the drivers mood will decrease a little bit")
     if max_distance >= distance:
         return True
     else:
