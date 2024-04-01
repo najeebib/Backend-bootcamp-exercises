c3e824a7b70bb5bda6d20e56f36544ffab1115a6
najeebib <najeebibraheem1999@hotmail.com>
2024-04-01 14:19:37 +0300

    Update students_route.py

--- Week 7/Day 1/basic crud server/routes/students_route.py
+++ Week 7/Day 1/basic crud server/routes/students_route.py
@@ -1,45 +1,59 @@
 from fastapi import APIRouter, HTTPException, Request, status
-import utils.data_handler as data_handler
+import utils.db_functions as db_fns
 from models.student_model import student_model
 from modules.student import Student
+import utils.auth_functions as auth_fns
 router = APIRouter()
 
 
-@router.get('/students')
-def get_stidents():
+@router.get('/school/students')
+def get_students(request: Request):
     # get all student from the db
-    return data_handler.get_students()
+    if auth_fns.check_token(request):
+        return db_fns.load_db('./data/students.json')
+    else:
+        raise HTTPException(400, "no token")
 
-@router.get('/students/{id}')
-def get_student(id: int):
-    students = data_handler.get_students()
-    for student in students:
-        # check if the stydents exists
-        if student["id"] == id:
-            return student
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No student with this id exists")
+@router.get('/school/students/{id}')
+def get_student(id: int, request: Request):
+    if auth_fns.check_token(request):
+
+        students = db_fns.load_db('./data/students.json')
+        for _, student_info in students.items():
+            if student_info["id"] == id:
+                return student_info
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No student with this id exists")
+    else:
+        raise HTTPException(400, "no token")
 
 
-@router.post('/students')
-def add_student(student: student_model):
-    # check if a student with this id allready exists in the db
-    students = data_handler.get_students()
-    for std in students:
-        # check if the stydents exists
-        if std["id"] == student.id:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Student with this id allready exists")
-    # make a new student object  then add it to the db
-    new_student = Student(student.name, student.id, student.age, student.classes)
-    data_handler.add_student(new_student)
-    return {"Stduent  has been added"}
+@router.post('/school/students')
+def add_student(student: student_model, request: Request):
+    if auth_fns.check_token_if_admin(request):
+        # check if a student with this id allready exists in the db
+        students = db_fns.load_db('./data/students.json')
+        for _, student_info in students.items():
+            if student_info["id"] == id:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Student with this id allready exists")
+        
+            # make a new student object  then add it to the db
+            new_student = Student(student.name, student.id, student.age, student.classes)
+            db_fns.add_student(new_student)
+            return {"Stduent  has been added"}
+    else:
+        raise HTTPException(400, "no token")
 
-@router.get("/class/{name}")
-def get_class(name: str):
-    students = data_handler.get_students()
-    students_in_class = []
-    # for each student check if they have the class in their classes list
-    for student in students:
-        if name in student["classes"]:
-            # add the student to list of students in this class
-            students_in_class.append(student)
-    return students_in_class
+@router.get("/school/class/{name}")
+def get_class(name: str, request: Request):
+    if auth_fns.check_token_if_admin(request):
+        students = db_fns.load_db('./data/students.json')
+        students_in_class = []
+        # for each student check if they have the class in their classes list
+        for _, student_info in students.items():
+            if name in student_info["classes"]:
+                # add the student to list of students in this class
+                students_in_class.append(student_info)
+        return students_in_class
+    else:
+        raise HTTPException(400, "no token")
