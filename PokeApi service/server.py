e7021c3eb2e9fde8992acd81ec2eb87c9197023b
najeebib <najeebibraheem1999@hotmail.com>
2024-04-16 12:45:24 +0300

    added pokemon api server

---
+++ PokeApi service/server.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from routes import routes
+
+app = FastAPI()
+
+app.include_router(routes.router)
+
+@app.middleware("http")
+async def log_req(request:Request, call_next):
+    print(f'got req. to: {request.url}, method: {request.method}')
+    response = await call_next(request)
+
+    return response
+
+@app.get("/")
+def root():
+    return "hi from fast api"
+
