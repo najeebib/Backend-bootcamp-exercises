c2721a85bf7d57811289b5f4c397d4005db585e6
najeebib <najeebibraheem1999@hotmail.com>
2024-04-19 19:56:24 +0300

    refactored code, added async await using httpx

--- PokeApi service/routes/routes.py
+++ PokeApi service/routes/routes.py
@@ -1,34 +1,34 @@
 from fastapi import APIRouter, HTTPException, Depends, Request
-import requests
+import httpx
+
 router = APIRouter()
 
 @router.get('/pokemon/{name}')
-def get_pokemon(name:str):
-    # todo: add async await to this, read HTTPX docoummentation and how to do it
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+async def get_pokemon(name:str):
+    async with httpx.AsyncClient() as client:
 
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Couldn't retrieve pokemon")
-    
-    data = response.json()
+        response = await client.get(f"https://pokeapi.co/api/v2/pokemon/{name.lower()}")
+        if response.status_code != 200:
+                    raise HTTPException(status_code=response.status_code, detail="Pokemon not found")
+        data = response.json()
 
-    pokemon_info = {
-        "name": data["name"].capitalize(),
-        "id": data["id"],
-        "types": [type_data["type"]["name"] for type_data in data["types"]],
-        "abilities": [ability_data["ability"]["name"] for ability_data in data["abilities"]],
-        "moves": [moves_data["move"]["name"] for moves_data in data["moves"]],
-        "species": data["species"]["name"],
-        "stats": {
-            "hp":  data["stats"][0]["base_stat"],
-            "attack":  data["stats"][1]["base_stat"],
-            "defense":  data["stats"][2]["base_stat"],
-            "special-attack":  data["stats"][3]["base_stat"],
-            "special-defense":  data["stats"][4]["base_stat"],
-            "speed":  data["stats"][5]["base_stat"],
+        pokemon_info = {
+            "name": data["name"].capitalize(),
+            "id": data["id"],
+            "types": [type_data["type"]["name"] for type_data in data["types"]],
+            "abilities": [ability_data["ability"]["name"] for ability_data in data["abilities"]],
+            "moves": [moves_data["move"]["name"] for moves_data in data["moves"]],
+            "species": data["species"]["name"],
+            "stats": {
+                "hp":  data["stats"][0]["base_stat"],
+                "attack":  data["stats"][1]["base_stat"],
+                "defense":  data["stats"][2]["base_stat"],
+                "special-attack":  data["stats"][3]["base_stat"],
+                "special-defense":  data["stats"][4]["base_stat"],
+                "speed":  data["stats"][5]["base_stat"],
 
+
+            }
         }
-    }
-
-    return pokemon_info
+        return pokemon_info
     
