a8b01a4fc9683f538a8cbfc1ef1d1897477097cd
najeebib <najeebibraheem1999@hotmail.com>
2024-05-20 15:46:44 +0300

    finished the get and delete routes

--- MySQL crud server/data/database.py
+++ MySQL crud server/data/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-URL_DATABASE = ''
+from typing import Annotated
+URL_DATABASE = 'mysql+pymysql://root:@localhost:3308/classicmodels'
 
 engine = create_engine(URL_DATABASE)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 base = declarative_base()
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
--- MySQL crud server/routes/customers_route.py
+++ MySQL crud server/routes/customers_route.py
@@ -1,23 +1,44 @@
-from fastapi import APIRouter, WebSocket,WebSocketDisconnect, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from models.customer_model import customer_model
+from sqlalchemy.orm import Session
+from data.database import get_db
+from sqlalchemy import text
+
 router = APIRouter()
 
 @router.get('/customers')
-def get_customers():
-    pass
+def get_customers(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT * FROM `customers`")).fetchall()
+    customers = []
+    for row in result:
+        customers.append(row._mapping)
+    return customers
 
-@router.get('/customers/{id}')
-def get_customer(id: int):
-    pass
+@router.get('/customers/{number}')
+def get_customer(number: int, db: Session = Depends(get_db)):
+    result = db.execute(text(f"SELECT * FROM `customers` WHERE customerNumber = {number}")).fetchone()
+    if not result:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return result._mapping
 
 @router.post('/customers')
 def add_customer(customer: customer_model):
     pass
 
-@router.put('/school/students/{id}')
-def edit_customer( id:int, customer: customer_model):
+@router.put('/customers{number}')
+def edit_customer( number:int, customer: customer_model):
     pass
 
-@router.delete('/school/students/{id}')
-def delete_customer( id: int):
-    pass
+@router.delete('/customers/{number}')
+def delete_customer( number: int, db: Session = Depends(get_db)):
+    result = db.execute(text(f"SELECT * FROM `customers` WHERE customerNumber = {number}")).fetchone()
+    if not result:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    try:
+        db.execute(text(f"DELETE FROM `customers` WHERE customerNumber = {number}"))
+        db.commit()
+    except Exception as e:
+        raise HTTPException(status_code=409, detail="Cannot delete customer: associated orders exist")
+
+
+    return {"msg": "Customer removed successfully"}
