98f96e46b7a492cd24d17c5cba9844b7981b012a
najeebib <najeebibraheem1999@hotmail.com>
2024-05-20 16:09:28 +0300

    added put and post methods

--- MySQL crud server/models/customer_model.py
+++ MySQL crud server/models/customer_model.py
@@ -1,3 +1,15 @@
 from pydantic import BaseModel
 class customer_model(BaseModel):
-    
+    customerName: str
+    contactLastName: str
+    contactFirstName: str
+    phone: str
+    addressLine1: str
+    addressLine2: str = None
+    city: str
+    state: str = None
+    postalCode: str = None
+    country: str
+    salesRepEmployeeNumber: int = None
+    creditLimit: float = None
+
--- MySQL crud server/routes/customers_route.py
+++ MySQL crud server/routes/customers_route.py
@@ -22,12 +22,84 @@ def get_customer(number: int, db: Session = Depends(get_db)):
     return result._mapping
 
 @router.post('/customers')
-def add_customer(customer: customer_model):
-    pass
+def add_customer(customer: customer_model, db: Session = Depends(get_db)):
+    try:
+        query = text("""
+            INSERT INTO `customers` 
+            (customerName, contactLastName, contactFirstName, phone, addressLine1, 
+            addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit) 
+            VALUES 
+            (:customerName, :contactLastName, :contactFirstName, :phone, :addressLine1, 
+            :addressLine2, :city, :state, :postalCode, :country, :salesRepEmployeeNumber, :creditLimit)
+        """)
+        db.execute(
+            query,
+            {
+                "customerName": customer.customerName,
+                "contactLastName": customer.contactLastName,
+                "contactFirstName": customer.contactFirstName,
+                "phone": customer.phone,
+                "addressLine1": customer.addressLine1,
+                "addressLine2": customer.addressLine2,
+                "city": customer.city,
+                "state": customer.state,
+                "postalCode": customer.postalCode,
+                "country": customer.country,
+                "salesRepEmployeeNumber": customer.salesRepEmployeeNumber,
+                "creditLimit": customer.creditLimit
+            }
+        )
+        db.commit()
+        return {"msg": "Customer created successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to create customer")
 
-@router.put('/customers{number}')
-def edit_customer( number:int, customer: customer_model):
-    pass
+@router.put('/customers/{number}')
+def edit_customer( number:int, customer: customer_model, db: Session = Depends(get_db)):
+    result = db.execute(text(f"SELECT * FROM `customers` WHERE customerNumber = {number}")).fetchone()
+    if not result:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    try:
+        query = text("""
+            UPDATE `customers` SET
+            customerName = :customerName, 
+            contactLastName = :contactLastName, 
+            contactFirstName = :contactFirstName, 
+            phone = :phone, 
+            addressLine1 = :addressLine1, 
+            addressLine2 = :addressLine2, 
+            city = :city, 
+            state = :state, 
+            postalCode = :postalCode, 
+            country = :country, 
+            salesRepEmployeeNumber = :salesRepEmployeeNumber, 
+            creditLimit = :creditLimit 
+            WHERE customerNumber = :customerNumber
+
+        """)
+        db.execute(
+            query,
+            {
+                "customerNumber": number,
+                "customerName": customer.customerName,
+                "contactLastName": customer.contactLastName,
+                "contactFirstName": customer.contactFirstName,
+                "phone": customer.phone,
+                "addressLine1": customer.addressLine1,
+                "addressLine2": customer.addressLine2,
+                "city": customer.city,
+                "state": customer.state,
+                "postalCode": customer.postalCode,
+                "country": customer.country,
+                "salesRepEmployeeNumber": customer.salesRepEmployeeNumber,
+                "creditLimit": customer.creditLimit
+            }
+        )
+        db.commit()
+        return {"msg": "Customer updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to update customer")
+    
 
 @router.delete('/customers/{number}')
 def delete_customer( number: int, db: Session = Depends(get_db)):
