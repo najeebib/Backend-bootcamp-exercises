a3ec51fb09821da274c078bd2ce8214d9d7fb089
najeebib <najeebibraheem1999@hotmail.com>
2024-06-04 15:54:54 +0300

    finished the mongodb api

---
+++ MongoDB server/mongoAPI.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+class MongoAPI:
+    def __init__(self, data) -> None:
+        self.client = MongoClient("mongodb://localhost:27017/")
+
+        database = data['database']
+        collection = data['collection']
+        cursor = self.client[database]
+        self.collection = cursor[collection]
+        self.data = data
+    
+    def read(self):
+        documents = self.collection.find()
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+    
+    def read_one(self, id: str):
+        documents = self.collection.find({'id': id})
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+    def write(self, data):
+        new_document = data
+        response = self.collection.insert_one(new_document)
+        output = {'Status': 'Successfully Inserted',
+                  'Document_ID': str(response.inserted_id)}
+        return output
+
+    def update(self,id, data):
+        filt = {"id":id}
+        updated_data = {"$set": data}
+        response = self.collection.update_one(filt, updated_data)
+        output = {'Status': 'Successfully Updated' if response.modified_count > 0 else "Nothing was updated."}
+        return output
+    
+    def delete(self, id):
+        filt = {"id":id}
+        response = self.collection.delete_one(filt)
+        output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
+        return output
+    
+
+mongo_api = MongoAPI({"database": "courses-service", "collection": "students"})
